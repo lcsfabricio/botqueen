17817307cf87d634c16adde2a7dc7e3c1fa5d893
lcsfabricio <47364675+lcsfabricio@users.noreply.github.com>
2019-02-05 11:36:04 -0800

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -7,7 +7,7 @@ class Development(Config):
     API_KEY = "730699562:AAFjF4ioNYf5PJaQX3fH_Iu-2MY4teNxuD0"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'https://vittarmodd.herokuapp.com/'  # sample db credentials
     MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
-    USE_MESSAGE_DUMP = True
+    USE_MESSAGE_DUMP = False
     SUDO_USERS = [168975623, 685446593]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['translation']
